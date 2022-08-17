8d4fa7fd0479425b593310e4ad38cb59f190032f
collinsmezie <collinschimezieobinwa@gmail.com>
2022-08-17 19:01:13 +0400

    add controllers spec

---
+++ spec/request/users_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before(:example) { get('/users') }
+    it 'should have an http success response' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'renders correct page contents' do
+      expect(response.body).to include('Users')
+    end
+    it 'renders index template' do
+      expect(response).to render_template('index')
+    end
+  end
+  describe 'GET #show' do
+    let(:user) do
+      User.create!(name: 'collins',
+                   photo: 'https://as2.ftcdn.net/v2/jpg/02/17/51/67/1000_F_217516770_nHjCK3C82B2ZUC3JB3qQs8W2BGLHxZfa.jpg',
+                   bio: 'Microverse_student', post_counter: 0)
+    end
+    before(:example) { get("/users/#{user.id}") }
+    it 'should have an http success response' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'renders correct page contents' do
+      expect(response.body).to include('collins')
+    end
+    it 'renders index template' do
+      expect(response).to render_template('show')
+    end
+  end
+end
