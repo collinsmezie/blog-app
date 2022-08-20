fd18f5670231ea56ce210ba23e8acd6b6898bf12
collinsmezie <collinschimezieobinwa@gmail.com>
2022-08-21 01:00:58 +0400

    add form to blog app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,26 @@ class PostsController < ApplicationController
   def show
     @user_post = Post.find(params[:id])
   end
+
+  def new
+    @new_post = Post.new
+  end
+
+  def create
+    post = params[:post]
+    user = User.find(params[:user_id])
+    new_post = Post.new(post.permit(:title, :text))
+    new_post.comment_counter = 0
+    new_post.like_counter = 0
+    new_post.author_id = user.id
+
+    if new_post.save
+      flash[:notice] = 'New post created successfully.'
+      redirect_to user_post_url(user, new_post)
+    else
+      flash[:error] = 'Creating new post failed!'
+      @post = new_post
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show] do 
-      resources :comments
-      resources :likes
+    resources :posts, only: [:index, :new, :create, :show] do 
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
     end
   end
 
   # Defines the root path route ("/")
-  # root to: "users#index"
+  root to: 'users#index'
 end
