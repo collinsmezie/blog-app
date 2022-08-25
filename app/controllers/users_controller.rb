3b9c8c16446129c158e09d677dfe6999a9a2d918
ichala <alijandhd@gmail.com>
2022-08-25 13:13:35 +0100

    Add n+1 solving and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.includes(:comments)
     @first_3_posts = @user.first_three_posts
     @last_3_posts = @user.last_three_posts
 
