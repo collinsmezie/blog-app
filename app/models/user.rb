f013fa8d68c54e447435cdae105a79543938fffd
collinsmezie <collinschimezieobinwa@gmail.com>
2022-08-28 00:05:16 +0400

    add authentication via devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, :photo, :bio, presence: true
 
-  validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
+  # validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def last_three_posts
     posts.order('created_at DESC').limit(3)
