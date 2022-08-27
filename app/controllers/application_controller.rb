7c0826fe310429fc896ad5c76a885ab02b16b0e1
collinsmezie <collinschimezieobinwa@gmail.com>
2022-08-28 00:11:43 +0400

    add authentication via devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:sign_up) | u |
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
+
+    devise_parameter_sanitizer.permit(:account_update) | u |
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
   end
 end
